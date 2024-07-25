3a4f4253e849780e155051aef3b7f75c41534b20
Cubebanyasz <59843249+misike12@users.noreply.github.com>
2024-07-25 14:16:45 +0200

    Remove old PWA stuff

--- public/assets/js/service.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register('service-worker.js').then(function(registration) {
-            console.log('ServiceWorker registered');
-          }).catch(function(err) {
-            console.log('ServiceWorker error: ', err);
-          });
-    })
-}
