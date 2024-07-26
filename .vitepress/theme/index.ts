9d83fba1cd9eac45421cb9ae677d231fff97f410
Cubebanyasz <59843249+misike12@users.noreply.github.com>
2024-07-26 14:12:21 +0200

    Update VitePress theme imports to optimize code organization and remove unnecessary imports.

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -7,42 +7,22 @@ import './style/style.css'
 import 'viewerjs/dist/viewer.min.css';
 import imageViewer from 'vitepress-plugin-image-viewer';
 import vImageViewer from 'vitepress-plugin-image-viewer/lib/vImageViewer.vue';
-
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import vitepressBackToTop from 'vitepress-plugin-back-to-top'
-
 import { NolebaseGitChangelogPlugin } from '@nolebase/vitepress-plugin-git-changelog/client'
 import '@nolebase/vitepress-plugin-git-changelog/client/style.css'
-
-import {
-  NolebasePagePropertiesPlugin,
-} from '@nolebase/vitepress-plugin-page-properties/client'
+import { NolebasePagePropertiesPlugin } from '@nolebase/vitepress-plugin-page-properties/client'
 import '@nolebase/vitepress-plugin-page-properties/client/style.css'
-
-import {  
-  NolebaseHighlightTargetedHeading,  
-} from '@nolebase/vitepress-plugin-highlight-targeted-heading/client'
+import { NolebaseHighlightTargetedHeading } from '@nolebase/vitepress-plugin-highlight-targeted-heading/client'
 import '@nolebase/vitepress-plugin-highlight-targeted-heading/client/style.css' 
-
-import {
-  NolebaseInlineLinkPreviewPlugin,
-} from '@nolebase/vitepress-plugin-inline-link-preview/client'
-
+import { NolebaseInlineLinkPreviewPlugin } from '@nolebase/vitepress-plugin-inline-link-preview/client'
 import '@nolebase/vitepress-plugin-inline-link-preview/client/style.css'
-
 import "vitepress-markdown-timeline/dist/theme/index.css";
-
 import type { Options } from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
 import { InjectionKey } from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
-
 import giscusTalk from 'vitepress-plugin-comment-with-giscus';
-
-import { 
-  NolebaseEnhancedReadabilitiesMenu, 
-  NolebaseEnhancedReadabilitiesScreenMenu, 
-} from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
+import { NolebaseEnhancedReadabilitiesMenu, NolebaseEnhancedReadabilitiesScreenMenu } from '@nolebase/vitepress-plugin-enhanced-readabilities/client'
 import '@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css'
-
 import Video from './components/Video.vue'
 import MNavLinks from './components/MNavLinks.vue'
 import Navlink from './components/Navlink.vue'
