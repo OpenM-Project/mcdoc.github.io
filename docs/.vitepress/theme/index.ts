3fea6c6ef920f94cf5b5143359961318eab968e9
Cubebanyasz <feleslegesfosfiok@gmail.com>
2024-10-13 12:25:00 +0200

    fix: update Giscus config

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -61,38 +61,26 @@ export default {
   },
 
   setup() {
-    // Get route
     const route = useRoute();
-    // Get frontmatter and route
     const { frontmatter } = toRefs(useData());
-    // Using
     imageViewer(route);
-    // Obtain configuration from: https://giscus.app/
     giscusTalk({
       repo: 'OpenM-Project/mcdoc.github.io',
-      repoId: 'R_kgDOMPBaCw',
-      category: 'General', // default: `General`
-      categoryId: 'DIC_kwDOMPBaC84CiDbA',
-      mapping: 'pathname', // default: `pathname`
-      inputPosition: 'top', // default: `top`
-      lang: 'en', // default: `zh-CN`
-      // i18n setting (Note: This configuration will override the default language set by lang)
-      // Configured as an object with key-value pairs inside:
-      // [your i18n configuration name]: [corresponds to the language pack name in Giscus]
+      repoId: 'R_kgDOM_hm8w',
+      category: 'General',
+      categoryId: 'DIC_kwDOM_hm884CjUWV',
+      mapping: 'pathname',
+      inputPosition: 'top',
+      lang: 'en',
       locales: {
           'en-US': 'en'
       },
-      homePageShowComment: false, // Whether to display the comment area on the homepage, the default is false
-      lightTheme: 'light', // default: `light`
-      darkTheme: 'transparent_dark', // default: `transparent_dark`
-      // ...
+      homePageShowComment: false,
+      lightTheme: 'light',
+      darkTheme: 'transparent_dark',
   }, {
       frontmatter, route
   },
-      // Whether to activate the comment area on all pages.
-      // The default is true, which means enabled, this parameter can be ignored;
-      // If it is false, it means it is not enabled.
-      // You can use `comment: true` preface to enable it separately on the page.
       true
   );
 },
